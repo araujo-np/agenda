57fd78b37e97550a16cddd0544a83c55b2002081
araujo-np <nathaly.paula@aluno.senai.br>
2026-01-27 10:15:26 -0300

    agora vai

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/ola')
+def ola_mundo():
+    return "Olá, Mundo!"
