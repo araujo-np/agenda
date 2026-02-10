9092b82a2b4dfdfd2739492826cf69262f366d0c
araujo-np <nathaly.paula@aluno.senai.br>
2026-02-10 16:37:08 -0300

    tarefa

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask.cli import F
 from models.tarefa import Tarefa
+from models.database import init_db
 
 app = Flask(__name__)
+
+init_db()
 
 @app.route('/')
 def home():
@@ -19,17 +21,18 @@ def agenda():
         tarefa.salvar_tarefa()
 
     tarefas = Tarefa.obter_tarefas()
-    return render_template('agenda.html', titulo='Agenda', tarefas=tarefas)
+    return render_template('agenda.html', titulo='Nova tarefa', tarefas=tarefas)
 
 @app.route('/delete/<int:idTarefa>')
 def delete(idTarefa):
     tarefa = Tarefa.id(idTarefa)
     tarefa.excluir_tarefa()
+    # return render_template('agenda.html', titulo="Agenda", tarefas=tarefas)
     return redirect(url_for('agenda'))
-    # return render_template('agenda.html',titulo="Agenda", tarefas=tarefas)
 
-@app.route('/update/<int:idTarefa>', methods=['GET', 'POST'])
+@app.route('/update/<int:idTarefa>', methods = ['GET', 'POST'])
 def update(idTarefa):
+    
     if request.method == 'POST':
         titulo = request.form['titulo-tarefa']
         data = request.form['data-conclusao']
@@ -37,9 +40,10 @@ def update(idTarefa):
         tarefa.atualizar_tarefa()
         return redirect(url_for('agenda')) # early return
 
-    tarefas = Tarefa.obter_tarefas()    
-    return render_template('agenda.html', titulo=f'Editando a tarefa ID: {idTarefa}', tarefas=tarefas)
+    tarefas = Tarefa.obter_tarefas()
+    tarefa_selecionada = Tarefa.id(idTarefa) # seleção da tarefa que será editada
 
+    return render_template('agenda.html', titulo=f'Editando a tarefa ID: {idTarefa}', tarefas=tarefas, tarefa_selecionada=tarefa_selecionada)
 
 @app.route('/ola')
 def ola_mundo():
--- models/tarefa.py
+++ models/tarefa.py
@@ -3,8 +3,10 @@ from typing import Self, Any, Optional
 from sqlite3 import Cursor
 
 class Tarefa:
-    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: Optional[str], 
-        id_tarefa: Optional[int] = None) -> None:
+    """
+        Classe para representar uma tarefa, com métodos para salvar, obter, excluir e atualizar tarefas em um banco de dados usando a classe `Database`.
+    """
+    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: Optional[str] = None, id_tarefa: Optional[int] = None) -> None:
         self.titulo_tarefa: Optional[str] = titulo_tarefa
         self.data_conclusao: Optional[str] = data_conclusao
         self.id_tarefa: Optional[int] = id_tarefa
@@ -16,7 +18,7 @@ class Tarefa:
 
     @classmethod
     def id(cls, id: int) -> Self:
-        with Database('./data/tarefas.sqlite3') as db:
+        with Database() as db:
             query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
             params: tuple = (id,)
             resultado: list[Any] = db.buscar_tudo(query, params)
@@ -25,7 +27,7 @@ class Tarefa:
             # Desempacotamento de coleção
             [[titulo, data]] = resultado
             
-    return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data)
+        return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data)
     
     # Simulando o conceito de sobrecarga
     # Tarefa('Título da Tarefa')
@@ -33,25 +35,30 @@ class Tarefa:
     # Tarefa.id(1)
 
     def salvar_tarefa(self: Self) -> None:
-        with Database('./data/tarefas.sqlite3') as db:
+        with Database() as db:
             query: str = "INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
             params: tuple = (self.titulo_tarefa, self.data_conclusao)
             db.executar(query, params)
 
     @classmethod
     def obter_tarefas(cls) -> list[Self]:
-        with Database('./data/tarefas.sqlite3') as db:
+        with Database() as db:
             query: str = 'SELECT titulo_tarefa, data_conclusao, id FROM tarefas;'
             resultados: list[Any] = db.buscar_tudo(query)
             tarefas: list[Self] = [cls(titulo, data, id) for titulo, data, id in resultados]
             return tarefas
         
     def excluir_tarefa(self) -> Cursor:
-        with Database('./data/tarefas.sqlite3') as db:
+        with Database() as db:
             query: str = 'DELETE FROM tarefas WHERE id = ?;'
             params: tuple = (self.id_tarefa,)
             resultado: Cursor = db.executar(query, params)
             return resultado
+        
+    def atualizar_tarefa(self) -> Cursor:
+        with Database() as db:
+            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ? WHERE id = ?;'
+            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
+            resultado: Cursor = db.executar(query, params)
+            return resultado
     
-    def atualizar_tarefa():
-        with Database
