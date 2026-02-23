dc9e787b57c1683d240f9176bc8509f7b5e8c1c5
araujo-np <nathaly.paula@aluno.senai.br>
2026-02-23 12:59:29 -0300

    agenda botão concluído

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 from models.database import Database
 
 with Database('./data/tarefas.sqlite3') as db:
-    db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);',
-     ('usar o gerenciador de contexto', '2026-02-03'))
+    db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?,?);', 
+    ('Usar o gerenciador de contexto', '2026-02-02')
+)
